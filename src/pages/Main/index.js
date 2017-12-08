b278f75ba38b09509a5c22b4ed08cdbf87e1ff0d
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2017-12-08 10:52:54 -0800

    imported favourites comp and added search and add repository

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -7,9 +7,15 @@ import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import Button from 'components/Button';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+/* Redux */
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import * as actions from 'redux/actions/favourites';
+
 import styles from './styles';
 
-export default class Main extends Component {
+class Main extends Component {
   static navigationOptions = {
     header: null,
   };
@@ -18,6 +24,18 @@ export default class Main extends Component {
     navigation: PropTypes.shape({
       navigate: PropTypes.func,
     }).isRequired,
+    searchAndAddRepository: PropTypes.func.isRequired,
+    favourites: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+    })).isRequired,
+  };
+
+  state = {
+    newRepositoryName: '',
+  };
+
+  addNewFavourite = () => {
+    this.props.searchAndAddRepository(this.state.newRepositoryName);
   };
 
   render() {
@@ -29,23 +47,25 @@ export default class Main extends Component {
         <View style={styles.formContainer}>
           <TextInput
             style={styles.input}
-            placeholder="Digite o nome do repositório"
+            placeholder="Insert the repository name"
             autoCapitalize="none"
             autoCorrect={false}
+            value={this.state.newRepositoryName}
+            onChangeText={text => this.setState({ newRepositoryName: text })}
           />
           <Button
             style={styles.button}
             color="success"
-            onPress={() => {}}
+            onPress={this.addNewFavourite}
           >
-            Adicionar repositório
+            Add New Repository
           </Button>
         </View>
 
         <View style={styles.userInformation}>
           <TouchableOpacity activeOpacity={0.6} onPress={() => { navigate('Favorites') }}>
             <Text style={styles.favoritesText}>
-              MEUS FAVORITOS (14)
+              My Favourites({this.props.favourites.length});
             </Text>
           </TouchableOpacity>
         </View>
@@ -53,3 +73,12 @@ export default class Main extends Component {
     );
   }
 }
+
+const mapStateToProps = state => ({
+  favourites: state.favourites,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(actions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
