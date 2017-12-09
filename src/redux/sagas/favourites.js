cc0cd7492650b7fa672f60c0224d1540332e6d29
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2017-12-08 17:25:39 -0800

    added sagas favourites

---
+++ src/redux/sagas/favourites.js
@@ -0,0 +1,8 @@
+import api from 'services/api';
+import { call, put } from 'redux-saga/effects';
+
+export function* searchAndAddRepository(action) {
+  const response = yield call(api.get, `/repos/${action.payload.repositoryName}`);
+
+  yield put({ type: 'ADD_FAVOURITE', payload: { repository: response.data } });
+}
